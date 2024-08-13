502871af06fe2d9755dfda70e1f4306c64f4213a
KMnO4-zx <1021385881@qq.com>
2024-08-13 09:05:03 +0800

    feat: 更新YOLOv8模型配置文件和训练脚本

--- train.py
+++ train.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # 加载预训练模型
-    model = YOLO("./ultralytics/cfg/models/v8/yolov8-obb-attention-cot.yaml").load('./yolov8n-obb.pt')
+    model = YOLO("./ultralytics/cfg/models/v8/yolov8-attention-se.yaml").load('./yolov8n.pt')
     # 开始训练
     model.train(
-        data="/mnt/g/WorkSpace/dataset/yolo-oob/dataset-radar/data.yaml",
+        data="/mnt/g/WorkSpace/dataset/YOLOv8-loopy/dataset/loopy.yaml",
         epochs=200,
         imgsz=640,
         device="0",
