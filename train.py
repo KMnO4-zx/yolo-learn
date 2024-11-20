5308e4255dfab00340ced71d3eb57c007dcbc60e
KMnO4-zx <1021385881@qq.com>
2024-11-20 20:37:37 +0800

    Add tiny dataset

--- train.py
+++ train.py
@@ -2,11 +2,12 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # 加载预训练模型
-    model = YOLO("./ultralytics/cfg/models/v8/yolov8-attention-se.yaml").load('./yolov8n.pt')
+    model = YOLO("./ultralytics/cfg/models/v8/yolov8n-obb.yaml").load('./yolov8n.pt')
     # 开始训练
     model.train(
-        data="/mnt/g/WorkSpace/dataset/YOLOv8-loopy/dataset/loopy.yaml",
+        data="/mnt/g/WorkSpace/github_project/yolo-learn/data/data.yaml",
         epochs=200,
         imgsz=640,
+        batch=4,
         device="0",
     )
