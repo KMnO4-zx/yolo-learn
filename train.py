bc41b6a2f976d91b78fb96dd0d566cfc1a3afbc0
KMnO4-zx <1021385881@qq.com>
2024-11-23 00:00:31 +0800

    feat: 更新训练脚本以使用新的模型配置和数据集路径

--- train.py
+++ train.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # 加载预训练模型
-    model = YOLO("./ultralytics/cfg/models/v8/yolov8n-obb.yaml").load('./yolov8n.pt')
+    model = YOLO("./ultralytics/cfg/models/v8/yolov8n.yaml").load('./yolov8n.pt')
     # 开始训练
     model.train(
-        data="/mnt/g/WorkSpace/github_project/yolo-learn/data/data.yaml",
+        data="./data/coco8/coco8.yaml",
         epochs=200,
         imgsz=640,
         batch=4,
