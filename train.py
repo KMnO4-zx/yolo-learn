0e7fd89c900a73c321f7c04e6b2717733c204551
KMnO4-zx <1021385881@qq.com>
2024-11-23 09:41:45 +0800

    update requirements.txt

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # 加载预训练模型
-    model = YOLO("./ultralytics/cfg/models/v8/yolov8n.yaml").load('./yolov8n.pt')
+    model = YOLO("./ultralytics/cfg/models/v8/yolov8n.yaml")
     # 开始训练
     model.train(
         data="./data/coco8/coco8.yaml",
